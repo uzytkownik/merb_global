de80c19851fdb45efd4cc493a700600f69149615
Maciej Piechotka <uzytkownik2@gmail.com>
2008-12-16 00:07:43 +0100

    Added simple iteration over CLDR.

--- rake/cldr.rb
+++ rake/cldr.rb
@@ -19,13 +19,51 @@ namespace :cldr do
   task :download
 
   
-  if File.file?("#{pwd}/tmp/cldr-core-1.6.1.zip")
+  unless File.file?("#{pwd}/tmp/cldr-core-1.6.1.zip")
     task :download => [:force_download]
   end
 
   desc "Process CLDR"
   task :process => [:download] do
-    # TODO: Implement
+    require 'rexml/document'
+    require 'pstore'
+    require 'fileutils'
+    
+    FileUtils.mkdir_p [pwd, 'data', 'data/cldr']
+    Dir["#{pwd}/tmp/cldr-core/main/*.xml"].each do |file|
+      puts "Processing #{file}"
+      open(file) do |file|
+        xml = REXML::Document.new(file)
+        
+        lang = xml.elements["/ldml/identity/language"].attributes["type"]
+        terr = xml.elements["/ldml/identity/territory"]
+        unless terr.nil?
+          terr = terr.attributes["type"]
+        end
+        script = xml.elements["/ldml/identity/script"]
+        unless script.nil?
+          script = script.attributes["type"]
+        end
+
+        if terr.nil?
+          if script.nil?
+            cldr = PStore.new("#{pwd}/data/cldr/#{lang}.pstore")
+          else
+            cldr = PStore.new("#{pwd}/data/cldr/#{lang}-#{script}.pstore")
+          end
+        else
+          if script.nil?
+            cldr = PStore.new("#{pwd}/data/cldr/#{lang}-#{terr}.pstore")
+          else
+            cldr = PStore.new("#{pwd}/data/cldr/#{lang}-#{script}-#{terr}.pstore")
+          end
+        end
+        
+        cldr.transaction do
+          
+        end
+      end
+    end
   end
 end
 
