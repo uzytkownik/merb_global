cda799f644eaf3275b409e2f7188c691d2b70f25
Maciej Piechotka <uzytkownik2@gmail.com>
2008-12-20 21:45:02 +0100

    Refactor of CLDR

--- rake/cldr.rb
+++ rake/cldr.rb
@@ -1,10 +1,70 @@
+CLDR_XML='tmp/cldr-core'
+CLDR_PSTORE='data/cldr'
+
+def parse_cldr(locale)
+  if File.file? "#{CLDR_PSTORE}/#{locale.gsub /_/, '-'}.pstore"
+    return PStore.new("#{CLDR_PSTORE}/#{locale.gsub /_/, '-'}.pstore")
+  end
+  
+  puts "Parsing #{locale}"
+  xml = REXML::Document.new(open("#{CLDR_XML}/main/#{locale}.xml"))
+        
+  lang = xml.elements['/ldml/identity/language'].attributes['type']
+  terr = xml.elements['/ldml/identity/territory']
+  unless terr.nil?
+    terr = terr.attributes['type']
+  end
+  script = xml.elements['/ldml/identity/script']
+  unless script.nil?
+    script = script.attributes['type']
+  end
+  
+  if terr.nil?
+    if script.nil?
+      cldr = PStore.new("#{CLDR_PSTORE}/#{lang}.pstore")
+      parent = nil
+    else
+      cldr = PStore.new("#{CLDR_PSTORE}/#{lang}-#{script}.pstore")
+      parent = nil
+    end
+  else
+    if script.nil?
+      cldr = PStore.new("#{CLDR_PSTORE}/#{lang}-#{terr}.pstore")
+      parent = parse_cldr(lang)
+    else
+      cldr = PStore.new("#{CLDR_PSTORE}/#{lang}-#{script}-#{terr}.pstore")
+      parent = parse_cldr("#{lang}_#{script}")
+    end
+  end
+
+  
+  if xml.elements['/ldml/alias/'].nil?
+    cldr.transaction do
+      cldr[:numeric] = {}
+      
+    end
+  else
+    alias_element = xml.elements['/ldml/alias/']
+    alias_cldr = parse_cldr(alias_element.attributes['source'])
+    alias_cldr.transaction(true) do
+      cldr.transaction do
+        alias_cldr.roots.each do |root|
+          cldr[root] = alias_cldr[root]
+        end
+      end
+    end
+  end
+
+  return cldr
+end
+
 namespace :cldr do
   desc "Force download and unzip of CLDR files"
   task :force_download do
     require 'net/http'
     require 'fileutils'
     
-    FileUtils.mkdir_p [pwd, 'tmp', 'cldr-core']
+    FileUtils.mkdir_p CLDR_XML
     Net::HTTP.start('unicode.org') do |http|
       resp = http.get('/Public/cldr/1.6.1/core.zip')
       open("#{pwd}/tmp/cldr-core-1.6.1.zip", 'wb') do |file|
@@ -12,7 +72,7 @@ namespace :cldr do
       end
     end
 
-    sh %{unzip -qo #{pwd}/tmp/cldr-core-1.6.1.zip -d #{pwd}/tmp/cldr-core}
+    sh %{unzip -qo tmp/cldr-core-1.6.1.zip -d #{CLDR_XML}}
   end
 
   desc "Download and unzip CLDR files"
@@ -29,53 +89,13 @@ namespace :cldr do
     require 'pstore'
     require 'fileutils'
     
-    if File.directory? "#{pwd}/data/cldr"
-      FileUtils.rm_r "#{pwd}/data/cldr"
+    if File.directory? CLDR_PSTORE
+      FileUtils.rm_r CLDR_PSTORE
     end
-    FileUtils.mkdir_p [pwd, 'data', 'data/cldr']
+    FileUtils.mkdir_p CLDR_PSTORE
 
-    Dir["#{pwd}/tmp/cldr-core/main/*.xml"].each do |file|
-      puts "Processing #{file}"
-      open(file) do |file|
-        xml = REXML::Document.new(file)
-        
-        lang = xml.elements["/ldml/identity/language"].attributes["type"]
-        terr = xml.elements["/ldml/identity/territory"]
-        unless terr.nil?
-          terr = terr.attributes["type"]
-        end
-        script = xml.elements["/ldml/identity/script"]
-        unless script.nil?
-          script = script.attributes["type"]
-        end
-
-        if terr.nil?
-          if script.nil?
-            cldr = PStore.new("#{pwd}/data/cldr/#{lang}.pstore")
-          else
-            cldr = PStore.new("#{pwd}/data/cldr/#{lang}-#{script}.pstore")
-          end
-        else
-          if script.nil?
-            cldr = PStore.new("#{pwd}/data/cldr/#{lang}-#{terr}.pstore")
-          else
-            cldr = PStore.new("#{pwd}/data/cldr/#{lang}-#{script}-#{terr}.pstore")
-          end
-        end
-        
-        xml.elements.each('/*/alias') do |al|
-          source = REXML::Document.new(open("#{pwd}/tmp/cldr-core/main/#{al.attributes['source']}.xml"))
-          path = al.attributes['path'] || al.parent.xpath
-          source.elements.each('#{path}/*') do |e|
-            al.parent << e
-          end
-          al.parent.delete(al)
-        end
-
-        cldr.transaction do
-          
-        end
-      end
+    Dir["#{CLDR_XML}/main/*.xml"].each do |file|
+      parse_cldr(File.basename(file, '.xml'))
     end
   end
 
