87ce1c84def4fec97008e27b24e7e9788eb04680
Michael Johnston <lastobelus@mac.com>
2009-04-17 04:05:47 -0700

    added a haml parser

---
+++ lib/merb_global/parsers/haml_parser.rb
@@ -0,0 +1,40 @@
+# Haml gettext parser module
+#
+# http://pastie.org/445297
+require 'gettext/tools/rgettext'
+require 'gettext/parser/ruby'
+require 'haml'
+
+class Haml::Engine
+  # Overriden function that parses Haml tags
+  # Injects gettext call for plain text action.
+  def parse_tag(line)
+    tag_name, attributes, attributes_hash, object_ref, nuke_outer_whitespace,
+      nuke_inner_whitespace, action, value = super(line)
+    @precompiled << "_(\"#{value}\")\n" unless action || value.empty?
+    [tag_name, attributes, attributes_hash, object_ref, nuke_outer_whitespace,
+        nuke_inner_whitespace, action, value]
+  end
+  # Overriden function that producted Haml plain text
+  # Injects gettext call for plain text action.
+  def push_plain(text)
+    @precompiled << "_(\"#{text.strip}\")\n"
+  end
+end
+
+# Haml gettext parser
+module HamlParser
+  module_function
+
+  def target?(file)
+    File.extname(file) == ".haml"
+  end
+
+  def parse(file, ary = [])
+    haml = Haml::Engine.new(IO.readlines(file).join)
+    code = haml.precompiled.split(/$/)
+    GetText::RubyParser.parse_lines(file, code, ary)
+  end
+end
+
+GetText::RGetText.add_parser(HamlParser)
