f5b4611905632739f92215f98c0c765fc5a818cf
Maciej Piechotka <uzytkownik2@gmail.com>
2009-05-21 22:05:24 +0200

    Finished basic Java Data provider

--- lib/merb_global/date_providers/java.rb
+++ lib/merb_global/date_providers/java.rb
@@ -8,18 +8,26 @@ module Merb
         
         def localize(locale, date, format)
           if date.is_a? Time
-            date = [java.util.Date.new(date.to_i)].to_java
-            format format.gsub(/%(.)/) do |l|
-              if $1 == '%'
-                l
-              else
-                '%t' + $1
-              end
-            end
-            java.lang.String.format(locale.java_locale, format, date)
+            date = [java.util.Date.new(date.to_i*1000)].to_java
+          elsif date.respond_to? :to_time
+            date = date.to_time
+            date = [java.util.Date.new(date.to_i*1000)].to_java
+          elsif date.is_a? Date || date.is_a? DateTime
+            date = Time.local_time(date.year, date.month, date.day)
+            date = [java.util.Date.new(date.to_i*1000)].to_java
+          elsif date.respond_to? :to_java
+            data = [data.to_java].to_java
           else
             raise "Not yet supported anything else then Time"
           end
+          format format.gsub(/%(.)/) do |l|
+            if $1 == '%'
+              l
+            else
+              '%t' + $1
+            end
+          end
+          java.lang.String.format(locale.java_locale, format, date)
         end
       end
     end
